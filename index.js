2eb4d00ea7b6628428cc9dba2f921c7faa47c3e0
sharmabhi0911 <sharmabhishek0911@gmail.com>
2025-02-26 19:40:34 +0530

    frontend deployed

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:"https://study-notion-frontend-omega-lac.vercel.app",
 		credentials:true,
 	})
 )
